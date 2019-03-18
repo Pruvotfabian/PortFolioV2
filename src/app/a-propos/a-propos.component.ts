9adfca30155b4514e25ce61081812cd8e41fded6
Pruvotfabian <pruvotfabian@gmail.com>
2019-03-18 14:44:05 +0100

    Ajout de hobbies et responsive CV et page 404

--- src/app/a-propos/a-propos.component.ts
+++ src/app/a-propos/a-propos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-a-propos',
@@ -10,6 +11,25 @@ export class AProposComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('.A').click(function(){
+        $('.menuA').toggleClass('active');
+        $('.menuB').removeClass('active');
+
+      });
+      $('.B').click(function(){
+        $('.menuA').removeClass('active');
+        $('.menuB').toggleClass('active');
+
+      });
+
+      $('.but').click(function(){
+        $('.menuA').removeClass('active');
+        $('.menuB').removeClass('active');
+
+      });
+
+    });
   }
 
 }
